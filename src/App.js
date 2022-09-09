9d0720fba3defa84a13dd8bb5ff2c12eae9f936d
Toluwalade <olujimi14@gmail.com>
2022-09-09 14:13:58 +0100

    state-checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/navbar';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    fullName: "Toluwalade Olujimi",
+    bio: "Toluwalade is a student of the University of lagos. She is cuurently in her second year. She decided to learn Web Development since the school is currently on strike and so far it has been great. She is a member of RCCG and she is one who is learning to love God.",
+    imgSrc: "",
+    profession: "Student",
+    show: true,
+    time: 0,
+  };
+
+
+toggleShow = () => {
+  this.setState((prevProfile) => ({ show: !prevProfile.show }));
+  this.state.show
+    ? this.componentDidMount()
+    : this.setState({ time: 0 }) && this.componentWillUnmount();
+};
+
+componentDidMount() {
+  this.myInterval = setInterval(() => {
+    this.setState((prevState) => ({ time: prevState.time + 1 }));
+  }, 1000);
+}
+
+componentWillUnmount() {
+  clearInterval(this.myInterval);
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="screen">
+      <Navbar />
+      {this.state.show == true && (
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={require("./components/IMG_E5524.JPG")}
+                className="img-fluid rounded-start"
+                alt="Me"
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">{this.state.fullName}</h5>
+                <h6 className="card-title">{this.state.profession}</h6>
+                <p className="card-text">{this.state.bio}</p>
+                  <small className="text-muted">Last mounted: {this.state.time} seconds ago</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="btn-div">
+        <button type="button" class="btn btn-dark" onClick={this.toggleShow}>
+          {this.state.show == true ? "Hide" : "Show"}
+        </button>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <h3>PROFILE</h3>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
