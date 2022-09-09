40a306a2d989b4ec7e3022200f331c49ba5975de
Toluwalade <olujimi14@gmail.com>
2022-09-09 11:50:47 +0100

    react-checkpoint

---
+++ src/components/layout/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <footer style={{fontSize:20,marginTop:400}}>
+        Thank you for trusting us.
+      </footer>
+    );
+  }
+}
+export default Footer;
---
+++ src/components/layout/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+      return(
+        <header style={{fontSize:20,marginBottom:40,paddingRight:'79%'}}>
+          Hello, kindly fill the form below.
+        </header>
+      );
+    }
+  }
+  export default Header;
